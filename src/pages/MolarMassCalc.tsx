d023d8f451e78ec47d68201a7908654d4d3cf241
= <evy1339@gmail.com>
2025-06-15 14:40:56 -0300

    Tratamento de informações da calculadora de massa molar

--- src/pages/MolarMassCalc.tsx
+++ src/pages/MolarMassCalc.tsx
@@ -1,26 +1,73 @@
-import Header from '../components/Header';
-import { useState } from 'react';
-import elements from '../data/elements.json';
+import Header from "../components/Header";
+import { useState } from "react";
+import elements from "../data/elements.json";
 
-function MolarMassCalc(){
-    const [mol, setMol] = useState<string>("");
+type Element = {
+  atomicNumber: number;
+  symbol: string;
+  name: string;
+  atomicMass: number;
+  group: number;
+  period: number;
+  category: string | null;
+  state: string | null;
+  electronegativity: number | null;
+};
 
-    function calc(mol: string): string[] {
-        // let totalMass = 0;
-        const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-        let nmol: string[] = upperLetters.reduce((acc, letter) => { return acc.replace(`${letter}`, ` ${letter}`); }, mol).split(" ").filter((e) => e !== "");	
-        console.log(nmol);
-        return nmol;
-    }
+function MolarMassCalc() {
+  const [mol, setMol] = useState<string>("");
 
-    return (
-        <div className="molar-mass-calc-page">
-            <Header/>
-            <h1>Calculadora de Massa Molar</h1>
-            <input type="text" className="mol-input" value={mol} onChange={(e)=>{setMol(e.target.value);}} />
-            {mol}<br/>
-            {calc(mol)}
-        </div>
-    )
+  function calc(mol: string, elements: Element[]): number {
+    let nmol: string[] = [];
+    nmol = mol
+      .split("")
+      .map((e: string) => (e === e.toUpperCase() ? ` ${e}` : e))
+      .join("")
+      .split(" ")
+      .filter((e: string) => e !== "");
+    nmol = nmol.reduce((acc: string[], val) => {
+      if (!isNaN(Number(val))) {
+        if (acc.length > 0 && !isNaN(Number(acc[acc.length - 1]))) {
+          acc[acc.length - 1] = String(
+            Number(acc[acc.length - 1]) * 10 + Number(val)
+          );
+        } else {
+          acc.push(val);
+        }
+      } else {
+        acc.push(val);
+      }
+      return acc;
+    }, []);
+    console.log(nmol);
+    let masses: number[] = [];
+    elements.forEach((element: Element) => {
+      nmol.forEach((symbol) => {
+        if (element.symbol === symbol) {
+          masses.push(element.atomicMass);
+        }
+      });
+    });
+    console.log(masses);
+    return masses.reduce((acc, curr) => acc + curr, 0);
+  }
+
+  return (
+    <div className="molar-mass-calc-page">
+      <Header />
+      <h1>Calculadora de Massa Molar</h1>
+      <input
+        type="text"
+        className="mol-input"
+        value={mol}
+        onChange={(e) => {
+          setMol(e.target.value);
+        }}
+      />
+      {mol}
+      <br />
+      {calc(mol, elements)}
+    </div>
+  );
 }
 export default MolarMassCalc;
