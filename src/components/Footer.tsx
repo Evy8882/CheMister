4e97a93ba89b8f381bc5eca548bf4243668fb3d6
Evy8882 <evy1339@gmail.com>
2026-09-15 19:58:48 -0300

    Refatora layout da homepage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,8 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faGraduationCap,
   faUsers,
-  faHeart,
   faFlask,
-  faPlus,
   faExternalLinkAlt
 } from "@fortawesome/free-solid-svg-icons";
 import logo from "../assets/Logos/4.png";
